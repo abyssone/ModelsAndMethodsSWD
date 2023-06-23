de7694135f012f07665cc85069fb9da897661c5a
abyssone <spenex@gmail.com>
2023-06-23 14:05:20 +0300

    feat: add task  2a to week 5

---
+++ module_2/week_5/task_2a/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        String[] splitInput;
+        Map<String, String[]> engLatDictionary = new HashMap<>();
+        Map<String, String[]> latEngDictionary = new HashMap<>();
+
+        while(!input.equals("0")){
+            input = scanner.nextLine();
+            splitInput = stringToKeyValues(input);
+            engLatDictionary.put(splitInput[0], Arrays.copyOfRange(splitInput, 1, splitInput.length));
+        }
+
+        for(Map.Entry<String, String[]> entry: engLatDictionary.entrySet()) {
+            for(String value: entry.getValue()){
+                if(latEngDictionary.containsKey(value)){
+                    latEngDictionary.put(value, append(latEngDictionary.get(value), entry.getKey()));
+                } else{
+                    latEngDictionary.put(value, new String[]{entry.getKey()});
+                }
+            }
+        }
+
+        System.out.println("eh");
+    }
+    private static String[] stringToKeyValues(String str){
+        String[] result = str.split("(\\s\\-\\s)|(\\,\\s)");
+        return result;
+    }
+    private static <T> T[] append(T[] arr, T element) {
+        final int N = arr.length;
+        arr = Arrays.copyOf(arr, N + 1);
+        arr[N] = element;
+        return arr;
+    }
+}
